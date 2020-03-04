d3028ae6d3f125b02654d3d2b4bd12ae5f56bbba
woduq1414 <jjy37777@naver.com>
2020-03-04 13:49:57 +0900

    asda

--- api/db.py
+++ api/db.py
@@ -3,8 +3,12 @@ from api.api import app
 
 import os
 import subprocess
-
+import socket
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('FLASK_TEST_DB', None)
-app.config['SQLALCHEMY_DATABASE_URI'] = subprocess.getstatusoutput('heroku config:get DATABASE_URL -a school-meal-with-react')[1]
+if (socket.gethostname() == 'DESKTOP-32A6L4O'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = subprocess.getstatusoutput('heroku config:get DATABASE_URL -a school-meal-with-react')[1]
+else:
+    app.config['SQLALCHEMY_DATABASE_URI']  = os.environ.get('DATABASE_URL', None)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
